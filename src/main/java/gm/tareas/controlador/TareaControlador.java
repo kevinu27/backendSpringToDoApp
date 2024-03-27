c86ea352f440888174fa7735c326c43b93649394
Kevin <kevin.sm89@gmail.com>
2024-03-27 15:52:52 +0100

    crud completo

--- src/main/java/gm/tareas/controlador/TareaControlador.java
+++ src/main/java/gm/tareas/controlador/TareaControlador.java
@@ -1,12 +1,17 @@
 package gm.tareas.controlador;
 
+import gm.tareas.excepcion.RecursoNoEncontradoExcepcion;
 import gm.tareas.modelo.Tarea;
 import gm.tareas.servicio.TareaServicio;
+//import org.hibernate.mapping.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -32,5 +37,37 @@ public class TareaControlador {
         logger.info("Tarea a agregar: " + tarea);
         return this.tareaServicio.guardarTarea(tarea);
     }
+    @GetMapping("/tarea/{id}")
+    public ResponseEntity<Tarea> obtenerTareaPorID(
+            @PathVariable int id){
+        Tarea tarea = this.tareaServicio.buscarTareaPorId(id);
+        logger.info("id de la tarea a obtern: " + id + tarea);
+        if(tarea != null)
+            return ResponseEntity.ok(tarea);
+        else
+            throw new RecursoNoEncontradoExcepcion("no se encontro el id " + id);
+    }
 
+    @PutMapping("/tarea/{id}")
+    public ResponseEntity<Tarea> actualizarTarea(
+            @PathVariable int id,
+            @RequestBody Tarea tareaRecibida
+    ){
+        Tarea tarea = this.tareaServicio.buscarTareaPorId(id);
+        tarea.setDescripcion(tareaRecibida.getDescripcion());
+        tarea.setCompletada(tareaRecibida.getCompletada());
+        tarea.setSubtarea(tareaRecibida.getSubtarea());
+        this.tareaServicio.guardarTarea(tarea);
+        return ResponseEntity.ok(tarea);
+    }
+
+    @DeleteMapping("/tarea/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarTarea(@PathVariable int id){
+        Tarea tarea = tareaServicio.buscarTareaPorId(id);
+        this.tareaServicio.eliminarTareaPorId(tarea.getIdTarea());
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("eliminado", Boolean.TRUE);
+        logger.info("id a elimar: " + id);
+        return ResponseEntity.ok(respuesta);
+    }
 }
